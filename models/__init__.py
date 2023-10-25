02bd0eb572e36c85c498181f7f2c62207f21d6b0
evertcolombia <evertescalante@gmail.com>
2023-10-24 19:51:23 -0500

    [feat]: import usser models on the _init__ file of the user/ module

--- models/__init__.py
+++
@@ -1,2 +0,0 @@
-from models.user.user_model import UserModel
-from models.user.user_type_model import UserTypeModel
---
+++ models/user/__init__.py
@@ -0,0 +1,4 @@
+"""
+"""
+from  .user_model import UserModel
+from  .user_type import  UserTypeModel
