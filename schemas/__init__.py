76f228dc2731c1704d9719fc9530c453decf80f0
evertcolombia <evertescalante@gmail.com>
2023-10-24 19:48:22 -0500

    [feat]: add schemas folder to represent entities layer on our project architecture

---
+++ schemas/__init__.py
@@ -0,0 +1,6 @@
+"""
+    this module contians all the schemas (data structures)
+    that will reflect our database models, when another layers
+    want to retrieve infromation from models, the schemas will
+    be in the middle to return pre defined data structures
+"""
